d68b9f1a95e009997723a0578501a49a50dd4d38
Hubertus Golf <info@berti-golf.de>
2018-11-04 13:02:09 +0100

    Feature adding maps to extension

--- Tests/Unit/Domain/Model/MapTest.php
+++ Tests/Unit/Domain/Model/MapTest.php
@@ -99,4 +99,231 @@ class MapTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
             $this->subject
         );
     }
+
+    /**
+     * @test
+     */
+    public function getEanReturnsInitialValueForInt()
+    {
+        self::assertSame(
+            0,
+            $this->subject->getEan()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setEanForIntSetsEan()
+    {
+        $this->subject->setEan(12);
+
+        self::assertAttributeEquals(
+            12,
+            'ean',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getKartenreiheReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getKartenreihe()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setKartenreiheForStringSetsKartenreihe()
+    {
+        $this->subject->setKartenreihe('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'kartenreihe',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getKartennummerReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getKartennummer()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setKartennummerForStringSetsKartennummer()
+    {
+        $this->subject->setKartennummer('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'kartennummer',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getTypReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getTyp()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setTypForStringSetsTyp()
+    {
+        $this->subject->setTyp('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'typ',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getMassstabReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getMassstab()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setMassstabForStringSetsMassstab()
+    {
+        $this->subject->setMassstab('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'massstab',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getPreisReturnsInitialValueForFloat()
+    {
+        self::assertSame(
+            0.0,
+            $this->subject->getPreis()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setPreisForFloatSetsPreis()
+    {
+        $this->subject->setPreis(3.14159265);
+
+        self::assertAttributeEquals(
+            3.14159265,
+            'preis',
+            $this->subject,
+            '',
+            0.000000001
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getVerlagReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getVerlag()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setVerlagForStringSetsVerlag()
+    {
+        $this->subject->setVerlag('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'verlag',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getLinkReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getLink()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setLinkForStringSetsLink()
+    {
+        $this->subject->setLink('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'link',
+            $this->subject
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getBildurlReturnsInitialValueForString()
+    {
+        self::assertSame(
+            '',
+            $this->subject->getBildurl()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setBildurlForStringSetsBildurl()
+    {
+        $this->subject->setBildurl('Conceived at T3CON10');
+
+        self::assertAttributeEquals(
+            'Conceived at T3CON10',
+            'bildurl',
+            $this->subject
+        );
+    }
 }
