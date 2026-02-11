9acf70c64a75aef1ed9b45e4d7ac2115853094d7
Hubertus Golf <bego68@users.noreply.github.com>
2026-02-11 20:09:06 +0100

    Enable recursive storage in TypoScript configuration

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -35,7 +35,7 @@ plugin.tx_skitourenrouten_skitouren {
     }
     persistence {
         storagePid = {$plugin.tx_skitourenrouten_skitouren.persistence.storagePid}
-        #recursive = 1
+        recursive = 1
     }
     features {
         #skipDefaultArguments = 1
